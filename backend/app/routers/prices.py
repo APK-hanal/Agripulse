44dba9bbd9d6a3e14e74cf7a7fb752aae3e6e14c
APK-hanal <apilkhanal10@gmail.com>
2026-04-28 19:42:41 +0545

    Added a get_commodity function

--- backend/app/routers/prices.py
+++ backend/app/routers/prices.py
@@ -4,3 +4,17 @@ from app.database import get_connection
 
 router = APIRouter()
 
+@router.get("/commodities")
+def get_commodities():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT canonical_name, category FROM commodity_map WHERE include = true ORDER BY canonical_name")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    formatted_data = []
+    for row in rows:
+        item = {"name" : row[0],
+         "category": row[1]}
+        formatted_data.append(item)
+    return formatted_data
