0b8e835b4647f9ad7baa53ccefaf2b33e0720ca8
APK-hanal <apilkhanal10@gmail.com>
2026-05-05 17:55:30 +0545

    Wrapped the seeder into functions

--- scripts/seed_db.py
+++ scripts/seed_db.py
@@ -6,45 +6,54 @@ from psycopg2.extras import execute_values
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from backend.app.database import get_connection
-conn = get_connection()
-cur = conn.cursor()
 
-file = pd.read_csv('data/parsed_prices.csv')
-df = pd.DataFrame(file)
-# Convert null to None
-df = df.where(pd.notna(df), other=None)
-df['date'] = pd.to_datetime(df['date']).dt.date
+def load_csv(file):
+    f = pd.read_csv(file)
+    df = pd.DataFrame(f)
+    # Convert null to None
+    df = df.where(pd.notna(df), other=None)
+    df['date'] = pd.to_datetime(df['date']).dt.date
+    return df
 
+def build_rows(df):
+    rows = []
+    for index,row in df.iterrows():
+        record = (
+            row['name_original'],
+            row['name'],
+            row['type'],
+            row['color'],
+            row['location'],
+            row['date'],
+            row['unit'],
+            row['maximum'],
+            row['minimum'],
+            row['average'],
+        )
+        rows.append(record)
+    return rows
+    
+def insert_to_db(rows):
+    conn = get_connection()
+    cur = conn.cursor()
+    try:
+        execute_values(cur, """
+            INSERT INTO prices_raw (name_original, name, type, color, location, date, unit, maximum, minimum, average)
+            VALUES %s
+            ON CONFLICT (name_original, date) DO NOTHING
+        """, rows)
+        conn.commit()
+        print(f"{cur.rowcount} rows inserted, {len(rows) - cur.rowcount} skipped as duplicates")
+        
+    except Exception as e:
+        conn.rollback()
+        print(f"Failed: {e}")
+        
+    finally:
+        cur.close()
+        conn.close()
 
-rows = []
-for index,row in df.iterrows():
-    record = (
-        row['name_original'],
-        row['name'],
-        row['type'],
-        row['color'],
-        row['location'],
-        row['date'],
-        row['unit'],
-        row['maximum'],
-        row['minimum'],
-        row['average'],
-    )
-    rows.append(record)
-    
-try:
-    execute_values(cur, """
-        INSERT INTO prices_raw (name_original, name, type, color, location, date, unit, maximum, minimum, average)
-        VALUES %s
-        ON CONFLICT (name_original, date) DO NOTHING
-    """, rows)
-    conn.commit()
-    print(f"{cur.rowcount} rows inserted, {len(rows) - cur.rowcount} skipped as duplicates")
-    
-except Exception as e:
-    conn.rollback()
-    print(f"Failed: {e}")
-    
-finally:
-    cur.close()
-    conn.close()
+if __name__ == "__main__":
+    df = load_csv('data/parsed_prices.csv')
+    rows = build_rows(df)
+    insert_to_db(rows)
